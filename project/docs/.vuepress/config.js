252df84347b142ac53288f22342ae1576f743c08
lemon.liao <lemon.liao@oneplus.net>
2019-07-17 10:33:37 +0800

    docs

---
+++ project/docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: "jxwaf",
+  description: "jxwaf文档",
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Guide", link: "/test1" },
+      { text: "External", link: "https://google.com" }
+    ],
+    sidebar: ["/", "/test1", ["/test1", "Explicit link text"]]
+  }
+};
